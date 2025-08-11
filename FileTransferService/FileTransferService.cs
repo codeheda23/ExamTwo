69cf8d475b0a1cabcf131764718ede51d09f620f
codeheda23@outlook.com <codeheda23@outlook.com>
2025-08-11 10:49:47 +0800

    Initial commit: setup project and  .gitignore

---
+++ FileTransferService/FileTransferService.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.IO;
+using System.ServiceProcess;
+using System.Threading;
+
+namespace ExamTwo.FileTransferService
+{
+    public partial class FileTransferService : ServiceBase
+    {
+        //If in a production environment, it's best to store and configure the hardcoded values below in an environment variable or app.Config
+
+        private const string SourceDir = @"C:\Folder1";
+        private const string DestinationDir = @"C:\Folder2";
+        private FileSystemWatcher _fsWatcher;
+
+        public FileTransferService()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            try
+            {
+                Directory.CreateDirectory(SourceDir);
+                Directory.CreateDirectory(DestinationDir);
+
+                _fsWatcher = new FileSystemWatcher(SourceDir)
+                {
+                    NotifyFilter = NotifyFilters.FileName,
+                    Filter = "*.*",
+                    EnableRaisingEvents = true
+                };
+
+                _fsWatcher.Created += OnFileCreated;
+
+                LogManager.Info($"Service Started. Monitoring: {SourceDir}");
+            }
+            catch (Exception ex)
+            {
+                LogManager.Error("Fatal error during service startup.", ex);
+                this.Stop();
+            }
+        }
+
+        protected override void OnStop()
+        {
+            if (_fsWatcher != null)
+            {
+                _fsWatcher.EnableRaisingEvents = false;
+                _fsWatcher.Dispose();
+            }
+            LogManager.Info("Service Stopped.");
+            LogManager.Shutdown();
+        }
+
+        private void OnFileCreated(object sender, FileSystemEventArgs e)
+        {
+            try
+            {
+                Thread.Sleep(500);
+
+                string destinationPath = Path.Combine(DestinationDir, e.Name);
+                File.Move(e.FullPath, destinationPath);
+
+                LogManager.Info($"Transferred file: '{e.Name}' to '{DestinationDir}'.");
+            }
+            catch (Exception ex)
+            {
+                LogManager.Error($"Error transferring file '{e.Name}'.", ex);
+            }
+        }
+
+
+    }
+}
