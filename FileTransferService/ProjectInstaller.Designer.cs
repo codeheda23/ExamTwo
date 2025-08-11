e0421890c3de6d12dc3775198653bffdfc760edf
codeheda23@outlook.com <codeheda23@outlook.com>
2025-08-11 10:53:31 +0800

    Feat: Add installer for FileTransferService

---
+++ FileTransferService/ProjectInstaller.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace ExamTwo.FileTransferService
+{
+    partial class ProjectInstaller
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.FileTransferServiceProcessInstaller = new System.ServiceProcess.ServiceProcessInstaller();
+            this.FileTransferServiceInstaller = new System.ServiceProcess.ServiceInstaller();
+            // 
+            // FileTransferServiceProcessInstaller
+            // 
+            this.FileTransferServiceProcessInstaller.Account = System.ServiceProcess.ServiceAccount.LocalService;
+            this.FileTransferServiceProcessInstaller.Password = null;
+            this.FileTransferServiceProcessInstaller.Username = null;
+            // 
+            // FileTransferServiceInstaller
+            // 
+            this.FileTransferServiceInstaller.Description = "File Transfer Service";
+            this.FileTransferServiceInstaller.DisplayName = "File Transfer Service";
+            this.FileTransferServiceInstaller.ServiceName = "File Transfer Service";
+            this.FileTransferServiceInstaller.StartType = System.ServiceProcess.ServiceStartMode.Automatic;
+            // 
+            // ProjectInstaller
+            // 
+            this.Installers.AddRange(new System.Configuration.Install.Installer[] {
+            this.FileTransferServiceProcessInstaller,
+            this.FileTransferServiceInstaller});
+
+        }
+
+        #endregion
+
+        private System.ServiceProcess.ServiceProcessInstaller FileTransferServiceProcessInstaller;
+        private System.ServiceProcess.ServiceInstaller FileTransferServiceInstaller;
+    }
+}
